a143dfb9a8172ace7e3314698af8c40208b7e8f9
Toyokaku Ryu <ryutoyokaku@gmail.com>
2021-05-03 01:47:45 -0700

    fix theme tests

--- themes/test/themes_test.dart
+++ themes/test/themes_test.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:songkoro_flutter/flutter.dart';
-import 'package:songkoro_flutter/utils/skr_theme.dart';
+import 'package:songkoro/flutter.dart';
 
 void main() {
   test('create all', () {
