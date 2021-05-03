873400fd07c6e57b205feb4c0007a89db38332c6
Toyokaku Ryu <ryutoyokaku@gmail.com>
2021-05-03 01:33:42 -0700

    songkoro theme manager

--- themes/test/themes_test.dart
+++ themes/test/themes_test.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:songkoro/flutter.dart';
+import 'package:songkoro_flutter/flutter.dart';
+import 'package:songkoro_flutter/utils/skr_theme.dart';
 
 void main() {
   test('create all', () {
     ThemeData materialTheme = SkrTheme.materialTheme();
-    CupertinoThemeData cupertinoTheme = SkrTheme.cupertinoTheme();
-    expect(materialTheme.brightness, Brightness.dark);
-    expect(cupertinoTheme.brightness, Brightness.dark);
   });
 }
