0fae415497aeb6d698ee75785646ce07ea1cda65
Toyokaku Ryu <ryutoyokaku@gmail.com>
2021-05-03 02:15:08 -0700

    fix test analysis

--- themes/test/themes_test.dart
+++ themes/test/themes_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:songkoro/flutter.dart';
@@ -6,5 +7,8 @@ import 'package:songkoro/flutter.dart';
 void main() {
   test('create all', () {
     ThemeData materialTheme = SkrTheme.materialTheme();
+    CupertinoThemeData cupertinoTheme = SkrTheme.cupertinoTheme();
+    expect(materialTheme.brightness, Brightness.dark);
+    expect(cupertinoTheme.brightness, Brightness.dark);
   });
 }
